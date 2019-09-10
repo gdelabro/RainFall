dd0b04808e88a737fb2ef0b5a034fb9ce1ec06e9
Guilhem DE LA BROSSE <gdelabro@e2r3p14.42.fr>
2019-09-10 14:31:09 +0200

    level8

--- level8/source.c
+++ level8/source.c
@@ -1,18 +1,48 @@
-void *ptr1 = 0; //0x08049ab0
-void *ptr2 = 0; //0x08049aac
-char buf[128];  //0x08049a80
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int ac, char **av)
+void *p1 = 0; //0x08049ab0
+void *p2 = 0; //0x08049aac
+
+int main(void)
 {
-	while (1)
+	char buf[128];
+
+	while(1)
 	{
-		printf("%p, %p \n", 0x08049ab0, 0x08049aac);
-		if (!fgets(buf, 128, stdin))
-			break ;
-		
+		int i;
+
+		printf("%p, %p \n", p1, p2);
+		if (fgets(buf, 128, stdin) == 0)
+			break;
+		if (strncmp(buf, "auth ", 5) == 0 && buf[5] != 0)
+		{
+			p1 = malloc(4);
+			if(strlen(buf+5) <= 30)
+			{
+				strcpy(p1, buf+5);
+			}
+		}
+		if (strncmp(buf, "reset", 5) == 0 && buf[5] != 0)
+		{
+			free(p1);
+		}
+		if (strncmp(buf, "service", 6) == 0 && buf[5] != 0)
+		{
+			p2 = strdup(buf+7);
+		}
+		if (strncmp(buf, "login", 5) == 0 && buf[5] != 0)
+		{
+			if (((char*)p1)[32])
+				system("/bin/sh");
+			else
+			{
+				fwrite ("Password:\n", 10, 1, stdout);
+			}
+		}
 	}
 }
-
 
 /*
  * 0x8048810     %p, %p \n
@@ -22,4 +52,4 @@ int main(int ac, char **av)
  * 0x804882d     login
  * 0x8048833     /bin/sh
  * 0x804883b     Password:\n
-*/
+ */
