88fcc7343502e2a3a7eff84ef1a2b5bb92132e23
Guilhem DE LA BROSSE <gdelabro@e2r3p12.42.fr>
2019-09-11 18:08:23 +0200

    level9

---
+++ level9/source.cpp
@@ -0,0 +1,19 @@
+
+class object
+{
+	void setAnnotation(char *ntm)
+	{
+
+		strlen(ntm);
+		memcpy();
+	};
+};
+
+int main(int ac, char **av)
+{
+	if (ac < 2)
+		exit(1);
+	object e;
+	e = _Znwj(108);
+	_ZN1NC1Ei(e, 5);
+}
